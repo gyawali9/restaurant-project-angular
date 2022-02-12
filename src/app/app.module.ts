b960eb91e7131d4d84785846e307e1828efcc871
gyawali9 <roshangyawali9@gmail.com>
2022-02-13 00:01:09 +0545

    Adding json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.component';
@@ -14,7 +14,8 @@ import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
