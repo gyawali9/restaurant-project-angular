7410908d6841e354977a261a5ec9a2e68e40f3c5
roshanIven <rgyawali@inventechgroup.com>
2022-02-21 22:03:57 +0545

    Adding data to server and retrieving to the UI

--- src/app/restaurant-dash/restaurant-dash.component.ts
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { RestaurantData } from './restaurant.model';
 
 @Component({
   selector: 'app-restaurant-dash',
@@ -8,8 +10,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class RestaurantDashComponent implements OnInit {
   formValue!: FormGroup;
+  restaurantModelObj: RestaurantData = new RestaurantData;
+  allRestaurantData:any;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private api:ApiService) { }
 
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
@@ -18,7 +22,39 @@ export class RestaurantDashComponent implements OnInit {
       mobile: [''],
       address: [''],
       services: ['']
-    });
+    })
+    this.getAllData();
+  }  
+
+  // Subscribing our data which is mapped through services
+  addResto(){
+    this.restaurantModelObj.name = this.formValue.value.name;
+    this.restaurantModelObj.email = this.formValue.value.email;
+    this.restaurantModelObj.mobile = this.formValue.value.mobile;
+    this.restaurantModelObj.address = this.formValue.value.address;
+    this.restaurantModelObj.services = this.formValue.value.services;
+
+    this.api.postRestaurant(this.restaurantModelObj).subscribe(res=>{
+      console.log(res);
+      alert('Restaurnt records added successfully');
+
+      // clear fill form data
+      let ref = document.getElementById('clear');
+      ref?.click();
+      this.formValue.reset();
+    },
+    err=>{
+      alert('Error in adding restaurant records');
+    }
+    )
   }
+
+  // Get all restaurant data
+  getAllData(){
+    this.api.getRestaurant().subscribe(res=>{
+      this.allRestaurantData = res;
+    })
+  }
+
 
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) {}
+    // Defining the POST, GET, PUT, DELETE
+
+    // Create Restaurent using Post method
+    postRestaurant(data:any){
+      return this._http.post<any>("http://localhost:3000/posts", data).pipe(map((res:any)=>{
+        return res;
+      }))
+    }
+
+    // Get restaurant data using Get Method
+    getRestaurant(){
+      return this._http.get<any>("http://localhost:3000/posts").pipe(map((res:any)=>{
+        return res;
+      }))
+    }
+
+    // Update Restaurant data using Put Method
+    updateRestaurant(data:any, id:number){
+      return this._http.put<any>("http://localhost:3000/posts/"+id, data).pipe(map((res:any)=>{
+        return res;
+      }))
+    }
+
+    // Delete Restaurant data using Delete Method
+    deleteRestaurant(id:number){
+      return this._http.delete<any>("http://localhost:3000/posts/"+id).pipe(map((res:any)=>{
+        return res;
+      }))
+    }
 }
